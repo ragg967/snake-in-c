ccf4d4a5035061cd28192b925cfb789f9ae65a52
Ragg967 <Ryewatson09@disroot.org>
2026-02-05 22:25:34 -0800

    dids te stuffs

--- src/main.c
+++ src/main.c
@@ -4,54 +4,69 @@
 
 #include "raylib.h"
 
-#define TILE_SCALE 2.0F
-#define TILE_SIZE (16.0F * TILE_SCALE)
-#define WIDTH (TILE_SIZE * 20.0F)
-#define HEIGHT (TILE_SIZE * 20.0F)
+#define TILE_SCALE 2
+#define TILE_SIZE (16 * TILE_SCALE)
+#define WIDTH (TILE_SIZE * 20)
+#define HEIGHT (TILE_SIZE * 20)
 
 struct Window {
     int width, height;
     char *name;
+    bool gameRunning;
 };
 
 struct Window window = {
     .width = WIDTH,
     .height = HEIGHT,
     .name = "Snake Game",
+    .gameRunning = true,
 };
 
 struct SnakeSegment {
     Vector2 position;
-    char *texturePath;
+    const char *texturePath;
     Texture texture;
 };
 
 struct Snake {
     unsigned int movementDirection: 2;
+    unsigned int segmentCount;
     struct SnakeSegment segments[];
 };
 
-struct Snake *InitSnake(char *texturePath, Texture texture);
+struct Snake *InitSnake(const char *texturePath, Texture texture);
 
 void drawSnake(const struct Snake *snake);
 
 void moveSnake(struct Snake *snake);
 
+void checkWallCollision(const struct Snake *snake);
+
 int main(void) {
     InitWindow(window.width, window.height, window.name);
     SetTargetFPS(5);
 
-    char *texturePath = "assets/snake-body.png";
+    const char *texturePath = "../assets/snake-body.png";
     const Texture texture = LoadTexture(texturePath);
 
-    struct Snake *snake = InitSnake(texturePath, texture);
+    if (!IsTextureValid(texture)) {
+        printf("\033[31mERROR: Texture could not be loaded\033[0m\n");
+        return 1;
+    }
 
-    while (!WindowShouldClose()) {
+    struct Snake *snake = InitSnake(texturePath, texture);
+    if (snake == NULL) {
+        printf("\033[31mERROR: Snake could not be loaded\033[0m\n");
+        return 1;
+    }
+
+    while (!WindowShouldClose() && window.gameRunning) {
         moveSnake(snake);
+        checkWallCollision(snake);
+
         BeginDrawing();
 
         ClearBackground(BLACK);
-        DrawRectangle(window.width / 2 - 25, window.height / 2 - 25, 50, 50, DARKGREEN);
         drawSnake(snake);
         DrawFPS(0, 0);
 
@@ -62,16 +77,17 @@ int main(void) {
     CloseWindow();
 }
 
-struct Snake *InitSnake(char *texturePath, const Texture texture) {
-    struct Snake *snake = malloc(sizeof(struct Snake) + (sizeof(struct SnakeSegment) * 10));
-    for (int i = 0; i < 10; i++) {
-        struct SnakeSegment newSnakeSegment = {
-            .position = (Vector2){i * TILE_SIZE, 0},
+struct Snake *InitSnake(const char *texturePath, const Texture texture) {
+    struct Snake *snake = malloc(sizeof(struct Snake) + (sizeof(struct SnakeSegment) * (WIDTH * HEIGHT)));
+    for (int i = 0; i < (WIDTH * HEIGHT); i++) {
+        const struct SnakeSegment newSnakeSegment = {
+            .position = (Vector2){((float)i * TILE_SIZE) + (TILE_SIZE * 7), (TILE_SIZE * 7)},
             .texturePath = texturePath,
             .texture = texture,
         };
         snake->segments[i] = newSnakeSegment;
     }
+    snake->segmentCount = 4;
     return snake;
 }
 
@@ -79,9 +95,10 @@ void drawSnake(const struct Snake *snake) {
     if (snake == NULL) {
         return;
     }
-    for (int i = 0; i < 10; i++) {
-        DrawRectangle(snake->segments[i].position.x, snake->segments[i].position.y, TILE_SIZE, TILE_SIZE, DARKGREEN);
+    for (int i = 1; i < snake->segmentCount; i++) {
+        DrawTextureRec(snake->segments[i].texture, (Rectangle){snake->segments[i].position.x, snake->segments[i].position.y, TILE_SIZE, TILE_SIZE}, snake->segments[i].position, DARKGREEN);
     }
+    DrawTextureRec(snake->segments[0].texture, (Rectangle){snake->segments[0].position.x, snake->segments[0].position.y, TILE_SIZE, TILE_SIZE}, snake->segments[0].position, WHITE);
 }
 
 void moveSnake(struct Snake *snake) {
@@ -114,6 +131,24 @@ void moveSnake(struct Snake *snake) {
             snake->segments[0].position.x += TILE_SIZE;
             break;
         default:
-            return;
+            break;
+    }
+}
+
+void checkWallCollision(const struct Snake *snake) {
+    if (snake == NULL) {
+        return;
+    }
+    if (snake->segments[0].position.x == 0) {
+        window.gameRunning = false;
+    }
+    if (snake->segments[0].position.x + TILE_SIZE == WIDTH) {
+        window.gameRunning = false;
+    }
+    if (snake->segments[0].position.y == 0) {
+        window.gameRunning = false;
+    }
+    if (snake->segments[0].position.y + TILE_SIZE == HEIGHT) {
+        window.gameRunning = false;
     }
 }
